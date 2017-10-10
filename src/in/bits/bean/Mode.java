1dd63950fca031145524ac80577b1d70b59bc124
vidhya <vidhya4991@gmail.com>
2017-10-09 22:13:48 -0500

    status change

--- src/in/bits/bean/Mode.java
+++ src/in/bits/bean/Mode.java
@@ -1,7 +1,7 @@
 package in.bits.bean;
 
 public enum Mode {
-	LOW("LOW"), MEDIUM("MEDIUM"), HIGH("HIGH");
+	LOW("LOW-0% to 40%"), MEDIUM("MEDIUM-40% to 60%"), HIGH("HIGH-60% to 100%");
 	
 	private String mode;
 	private Mode(String mode){
